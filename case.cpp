8e291a807ae198222e4c5b16cf9a1808b9ab2f72
ktg0413 <kimteagyun0413@gmail.com>
2026-05-20 00:16:32 -0400

    switch

---
+++ case.cpp
@@ -0,0 +1,57 @@
+bool accept() {
+    cout<<"Do you want to proceed (y or n)?\n";
+
+    char answer = 0;
+    cin >> answer;
+
+    if (answer == 'y') {
+        return true;
+    }
+    return false;
+}
+
+bool accept2() {
+    cout<<"Do you want to proceed (y or n)?\n";
+
+    char answer =0;
+    cin >> answer;
+
+    switch (answer) {
+    case 'y':
+        return true;
+    case 'n':
+        return false;
+    default:
+        cout<<"I'll take that for a no.\n";
+        return false;
+    }
+}
+
+void action() {
+    while (ture) {
+        cout<<"enter action\n";
+        string act;
+        cin>>act;
+        Point delta {0,0}; // Point hold an {x,y} pair
+
+        for (char ch: act) {
+            switch(ch) {
+            case 'u':
+            case 'n':
+                ++delta.y;
+                break;
+            case 'r':
+            case 'e':
+                ++delta.x;
+                break
+            //...
+
+            default:
+                cout<<"I freeze!\n";
+            }
+            
+            move(current+delta*scale); // dont even explain what it is,,,
+            update_display()
+        }
+    }
+}
